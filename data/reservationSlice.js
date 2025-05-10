1e9fb6c7c397e0f910e830d743a8b2214dd8d76c
chritophe1213 <christophe1213.com@gmail.com>
2025-05-10 15:53:18 +0300

    update the page reservation

--- data/reservationSlice.js
+++ data/reservationSlice.js
@@ -2,15 +2,18 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = [
-  { id: "#RES-1001", spot: "A1", chek_in: "Apr 2, 2025 08:30 AM", check_out: "Apr 2, 2025 10:30 AM",status:'payé' },
-  { id: "#RES-1002", spot: "A2", chek_in: "Apr 3, 2025 09:00 AM", check_out: "Apr 3, 2025 11:00 AM",status:"non payé" },
-  { id: "#RES-1003", spot: "B1", chek_in: "Apr 4, 2025 07:15 AM", check_out: "Apr 4, 2025 09:15 AM",status:'payé' },
+  { id: "#RES-1001", spot: "A1", startTime: "Apr 2, 2025 08:30 AM", endTime: "Apr 2, 2025 10:30 AM",status:'payed' },
+  { id: "#RES-1002", spot: "A2", startTime: "Apr 3, 2025 09:00 AM", endTime: "Apr 3, 2025 11:00 AM",status:"unpayed" },
+  { id: "#RES-1003", spot: "B1", startTime: "Apr 4, 2025 07:15 AM", endTime: "Apr 4, 2025 09:15 AM",status:'payed' },
 ];
 
 const reservationSlice = createSlice({
   name: 'reservations',
   initialState,
   reducers: {
+    setReservation:(state,action)=>{
+      return action.payload
+    },
     deleteReservation: (state, action) => {
       return state.filter(res => res.id !== action.payload);
     },
--- src/components/MyReservation/ReservationList.jsx
+++ src/components/MyReservation/ReservationList.jsx
@@ -1,54 +1,39 @@
-// const reservations = [
-//   { parking: 'Parking Gare', date: '10/04/2025', status: '', debut: '', fin: '', prix: '' },
-//   { parking: 'Parking Centre-ville', date: '07/04/2025', status: '', debut: '', fin: '', prix: '' },
-//   { parking: 'Parking Centre-ville', date: '07/04/2025', status: '', debut: '', fin: '', prix: '' },
-// ];
-
-import ReservationCard from './ReservationCard';
 import { useSelector, useDispatch } from 'react-redux';
 import { deleteReservation } from '../../../data/reservationSlice';
-import { FaTimes } from 'react-icons/fa';
+import { useState } from 'react';
+
 const ReservationList = () => {
   const reservations = useSelector(state => state.reservations);
   const dispatch = useDispatch();
+  const handleDelete=(id)=>{
+    dispatch(deleteReservation(id))
+  }
+  const [search,setSearch]=useState('#RES-1001')
+  const filterRes=(reservations==[])?[]:reservations.filter(
+    r=>r.id.toLowerCase().includes(search.toLocaleLowerCase())||
+    r.spot.toLowerCase().includes(search.toLocaleLowerCase())||
+    r.status.toLowerCase().includes(search.toLocaleLowerCase())||
+    r.startTime.toLowerCase().includes(search.toLocaleLowerCase())||
+    r.endTime.toLowerCase().includes(search.toLocaleLowerCase())
+)
   return (
     <>
-      {/* <h1 class="text-xl font-semibold mb-6">Liste de toutes vos réservations</h1>
-      {reservations.map((r) => (
-        <ReservationCard reservation={r} />
-      ))} */}
-
+ 
       <div className=''>
         <div className="flex flex-col md:flex-row justify-between items-start md:items-center mb-6 gap-4">
-                  <h2 className="text-lg font-semibold text-gray-800">Recent Reservations</h2>
-                  <div className="flex flex-wrap items-center gap-2">
-                      <div className="relative">
-                          <button className="border border-gray-200 rounded-button px-3 py-1.5 text-sm text-gray-600 flex items-center whitespace-nowrap">
-                              <div className="w-4 h-4 flex items-center justify-center mr-1.5">
-                                  <i className="ri-filter-3-line"></i>
-                              </div>
-                              Filter
-                          </button>
-                      </div>
-                      <div className="relative">
-                          <button className="border border-gray-200 rounded-button px-3 py-1.5 text-sm text-gray-600 flex items-center whitespace-nowrap">
-                              <div className="w-4 h-4 flex items-center justify-center mr-1.5">
-                                  <i className="ri-download-line"></i>
-                              </div>
-                              Export
-                          </button>
-                      </div>
-                      <div className="relative">
-                          <button className="border border-gray-200 rounded-button px-3 py-1.5 text-sm text-gray-600 flex items-center whitespace-nowrap">
-                              <div className="w-4 h-4 flex items-center justify-center mr-1.5">
-                                  <i className="ri-printer-line"></i>
-                              </div>
-                              Print
-                          </button>
-                      </div>
-                  </div>
-            </div>
-            <div className="overflow-x-auto custom-scrollbar">
+            <h2 className="text-lg font-semibold text-gray-800">Recent Reservations</h2>
+        </div>
+          <header className="flex gap-2 mb-8">
+        <input
+          type="text"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          placeholder="Trouver un parking"
+          className="border border-gray-300 p-2 rounded w-full"
+        />
+        <button className="bg-blue-600 text-white px-4 py-2 rounded">Rechercher</button>
+      </header>
+        <div className="overflow-x-auto custom-scrollbar">
                 <table  className='w-full min-w-[800px] text-sm text-left'>
                     <thead className='text-xs text-gray-700 uppercase bg-gray-50'>
                       <tr>
@@ -58,30 +43,37 @@ const ReservationList = () => {
                             <th className="px-4 py-3 font-medium">Check-in</th>
                             <th className="px-4 py-3 font-medium">Check-out</th>
                             <th className="px-4 py-3 font-medium">Status</th>
-                            <th className="px-4 py-3 font-medium">Annuler</th>
+                            <th className="px-4 py-3 font-medium">Action</th>
                         </tr>
                     </thead>
                     <tbody>
-                        {reservations.map((r=>(
+                        {filterRes.map((r=>(
                           <tr  key={r.id} class="bg-white border-b hover:bg-gray-50">
                           <td className="px-4 py-3 font-medium text-gray-900">{r.id}</td>
 
                           <td className="px-4 py-3">{r.spot}</td>
-                          <td className="px-4 py-3">{r.chek_in}</td>
-                          <td className="px-4 py-3">{r.check_out}</td>
+                          <td className="px-4 py-3">{r.startTime}</td>
+                          <td className="px-4 py-3">{r.endTime}</td>
                           <td className="px-4 py-3">
                               <span className="px-2 py-1 text-xs font-medium rounded-full bg-yellow-100 text-yellow-800">{r.status}</span>
                           </td>
                           <td className="px-4 py-3">
-                              <div className="flex items-center space-x-2">
+                            
+                            <div className='flex items-center space-x-2'>
+                                <button className="bg-blue-500 text-white px-3 py-1 rounded">
                               
-                                  <button  className="text-gray-500 hover:text-red-500"
-                                    onClick={() => dispatch(deleteReservation(r.id))}
+                                    Pay
+                                </button>
+                                <button className="bg-gray-500 text-white px-3 py-1 rounded"
+                                  onClick={()=>handleDelete(r.id)}
                                   >
-                                     <FaTimes size={24} color="red" />
+                                    Cancel
                                   </button>
-                              </div>
+                        </div>
                           </td>
+                        <td>
+                  
+                        </td>
                       </tr>
                         )))}
                     </tbody>
