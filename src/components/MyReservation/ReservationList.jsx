e869f969399363dba4a9781042df26a2b3c7e5c0
chritophe1213 <christophe1213.com@gmail.com>
2025-05-11 10:13:50 +0300

    update Component MyReservation

--- src/components/MyReservation/ReservationList.jsx
+++ src/components/MyReservation/ReservationList.jsx
@@ -8,7 +8,9 @@ const ReservationList = () => {
   const handleDelete=(id)=>{
     dispatch(deleteReservation(id))
   }
-  const [search,setSearch]=useState('#RES-1001')
+  const [search,setSearch]=useState('')
+  
+  // Search
   const filterRes=(reservations==[])?[]:reservations.filter(
     r=>r.id.toLowerCase().includes(search.toLocaleLowerCase())||
     r.spot.toLowerCase().includes(search.toLocaleLowerCase())||
@@ -27,7 +29,7 @@ const ReservationList = () => {
           type="text"
           value={search}
           onChange={(e) => setSearch(e.target.value)}
-          placeholder="Trouver un parking"
+          placeholder=" Search for a  reservation"
           className="border border-gray-300 p-2 rounded w-full"
         />
         <button className="bg-blue-600 text-white px-4 py-2 rounded">Rechercher</button>
@@ -54,21 +56,26 @@ const ReservationList = () => {
                           <td className="px-4 py-3">{r.startTime}</td>
                           <td className="px-4 py-3">{r.endTime}</td>
                           <td className="px-4 py-3">
-                              <span className="px-2 py-1 text-xs font-medium rounded-full bg-yellow-100 text-yellow-800">{r.status}</span>
+                            {r.status.toLowerCase()==="payed"&&
+                              <span className="px-2 py-1 rounded bg-green-100 text-green-800">{r.status}</span> 
+                            }
+                            {r.status.toLowerCase()==="unpayed"&& 
+                                <span className="px-2 py-1 rounded bg-red-100 text-red-800">{r.status}</span>
+                            } 
+                             
                           </td>
                           <td className="px-4 py-3">
                             
                             <div className='flex items-center space-x-2'>
-                                <button className="bg-blue-500 text-white px-3 py-1 rounded">
-                              
-                                    Pay
-                                </button>
+                                <button class="bg-blue-500 text-white px-4 py-2 rounded disabled:opacity-50"  
+                                    disabled={r.status.toLowerCase()==="payed"} 
+                                >Pay</button>
                                 <button className="bg-gray-500 text-white px-3 py-1 rounded"
                                   onClick={()=>handleDelete(r.id)}
                                   >
                                     Cancel
-                                  </button>
-                        </div>
+                                </button>
+                            </div>
                           </td>
                         <td>
                   
@@ -78,50 +85,7 @@ const ReservationList = () => {
                     </tbody>
                 </table>
             </div>
-          </div>
-        </div>
-        <div className="overflow-x-auto custom-scrollbar">
-          <table className="w-full min-w-[800px] text-sm text-left">
-            <thead className="text-xs text-gray-700 uppercase bg-gray-50">
-              <tr>
-                <th className="px-4 py-3 font-medium">Booking ID</th>
-
-                <th className="px-4 py-3 font-medium">Spot</th>
-                <th className="px-4 py-3 font-medium">Check-in</th>
-                <th className="px-4 py-3 font-medium">Check-out</th>
-                <th className="px-4 py-3 font-medium">Status</th>
-                <th className="px-4 py-3 font-medium">Annuler</th>
-              </tr>
-            </thead>
-            <tbody>
-              {reservations.map((r) => (
-                <tr key={r.id} class="bg-white border-b hover:bg-gray-50">
-                  <td className="px-4 py-3 font-medium text-gray-900">{r.id}</td>
-
-                  <td className="px-4 py-3">{r.spot}</td>
-                  <td className="px-4 py-3">{r.chek_in}</td>
-                  <td className="px-4 py-3">{r.check_out}</td>
-                  <td className="px-4 py-3">
-                    <span className="px-2 py-1 text-xs font-medium rounded-full bg-yellow-100 text-yellow-800">
-                      {r.status}
-                    </span>
-                  </td>
-                  <td className="px-4 py-3">
-                    <div className="flex items-center space-x-2">
-                      <button
-                        className="text-gray-500 hover:text-red-500"
-                        onClick={() => dispatch(deleteReservation(r.id))}
-                      >
-                        <FaTimes size={24} color="red" />
-                      </button>
-                    </div>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      </div>
+          </div>      
     </>
   );
 };
