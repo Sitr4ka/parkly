9b53a2990b7f044c040c8ae0e03658609ed62ff4
sitr4ka <fifasitrakarkt@gmail.com>
2025-05-15 06:00:31 +0300

    feat: generate fake spot to complete booking registration

--- src/components/BookingModal.jsx
+++ src/components/BookingModal.jsx
@@ -3,6 +3,11 @@ import img from "../assets/img.svg";
 import { TiTick } from "react-icons/ti";
 
 const BookingModal = ({ showModal, selectedParking }) => {
+  const [formData, setFormData] = useState({
+    startDate: "",
+    endDate: "",
+    spot: "",
+  });
   const steps = ["schedule", "spot", "validation"];
   const [currentStep, setCurrentStep] = useState(1);
   const [isCompleted, setIsCompleted] = useState(false);
@@ -25,7 +30,7 @@ const BookingModal = ({ showModal, selectedParking }) => {
     <>
       {showModal && (
         <div className="fixed left-60 flex items-center justify-center inset-0 backdrop-blur-xs bg-black/10 z-40">
-          <div className="flex bg-amber-100 rounded-xl px-12 py-12 w-[850px] h-[500px] shadow-shadow z-50">
+          <div className="flex bg-amber-100 rounded-xl px-10 py-12 w-[850px] h-[520px] shadow-shadow z-50">
             <div className="pr-6 pl-12 pt-4 w-1/2 flex flex-col justify-evenly">
               <h1 className="text-5xl font-bold">{selectedParking.name}</h1>
               <img src={img} alt="parking-img" />
@@ -47,8 +52,8 @@ const BookingModal = ({ showModal, selectedParking }) => {
                 ))}
               </div>
 
-              <div className="mx-6 mt-8">
-                {currentStep == 1 && <Schedule />}
+              <div className="mx-6 mt-5">
+                {currentStep == 1 && <Schedule data={formData} />}
                 {currentStep == 2 && <Spot />}
                 {currentStep == 3 && <Validation />}
               </div>
@@ -77,15 +82,17 @@ const BookingModal = ({ showModal, selectedParking }) => {
 
 export default BookingModal;
 
-const Schedule = () => {
+const Schedule = ({ data }) => {
+  const { startDate, endDate, spot } = data;
   return (
     <>
       <form method="post">
         <div className="input-group mb-4">
-          <label className="block text-sm font-medium mb-1">Date</label>
+          <label className="block text-sm font-medium mb-1">Start Date</label>
           <input
             type="date"
             className="w-full px-3 py-2 border border-gray-300 rounded-md bg-blue-200"
+            value="2025-05-05"
           />
         </div>
         <div className="grid grid-cols-2 gap-3">
@@ -94,6 +101,7 @@ const Schedule = () => {
             <input
               type="time"
               className="w-full px-3 py-2 border border-gray-300 rounded-md bg-blue-200"
+              value="17:00"
             />
           </div>
           <div className="input-group mb-4">
@@ -101,6 +109,7 @@ const Schedule = () => {
             <input
               type="time"
               className="w-full px-3 py-2 border border-gray-300 rounded-md bg-blue-200"
+              value="20:00"
             />
           </div>
         </div>
@@ -112,7 +121,24 @@ const Schedule = () => {
 const Spot = () => {
   return (
     <>
-      <h1 className="text-3xl">Spot</h1>
+      <div className="flex flex-wrap items-start justify-center gap-3 border px-3 py-2">
+        <div className="w-12 text-sm px-1 py-2 hover:bg-green-200 cursor-pointer border rounded-md text-center">R1-C1</div>
+        <div className="w-12 text-sm px-1 py-2 hover:bg-green-200 cursor-pointer border rounded-md text-center">R1-C2</div>
+        <div className="w-12 text-sm px-1 py-2 hover:bg-green-200 cursor-pointer border rounded-md text-center">R1-C3</div>
+        <div className="w-12 text-sm px-1 py-2 hover:bg-green-200 cursor-pointer border rounded-md text-center">R1-C4</div>
+        <div className="w-12 text-sm px-1 py-2 hover:bg-green-200 cursor-pointer border rounded-md text-center">R2-C1</div>
+        <div className="w-12 text-sm px-1 py-2 hover:bg-green-200 cursor-pointer border rounded-md text-center">R2-C2</div>
+        <div className="w-12 text-sm px-1 py-2 hover:bg-green-200 cursor-pointer border rounded-md text-center">R2-C3</div>
+        <div className="w-12 text-sm px-1 py-2 hover:bg-green-200 cursor-pointer border rounded-md text-center">R2-C4</div>
+        <div className="w-12 text-sm px-1 py-2 hover:bg-green-200 cursor-pointer border rounded-md text-center">R3-C1</div>
+        <div className="w-12 text-sm px-1 py-2 hover:bg-green-200 cursor-pointer border rounded-md text-center">R3-C2</div>
+        <div className="w-12 text-sm px-1 py-2 hover:bg-green-200 cursor-pointer border rounded-md text-center">R3-C3</div>
+        <div className="w-12 text-sm px-1 py-2 hover:bg-green-200 cursor-pointer border rounded-md text-center">R3-C4</div>
+        <div className="w-12 text-sm px-1 py-2 hover:bg-green-200 cursor-pointer border rounded-md text-center">R4-C1</div>
+        <div className="w-12 text-sm px-1 py-2 hover:bg-green-200 cursor-pointer border rounded-md text-center bg-green-500">R4-C2</div>
+        <div className="w-12 text-sm px-1 py-2 hover:bg-green-200 cursor-pointer border rounded-md text-center">R4-C3</div>
+        <div className="w-12 text-sm px-1 py-2 hover:bg-green-200 cursor-pointer border rounded-md text-center">R4-C4</div>
+      </div>
     </>
   );
 };
@@ -120,7 +146,50 @@ const Spot = () => {
 const Validation = () => {
   return (
     <>
-      <h1 className="text-3xl">Validation</h1>
+      <div className="flex justify-between items-center mb-2">
+        <label className="">Date:</label>
+        <input
+          type="date"
+          className="w-2/3 px-3 py-2 border border-gray-300 rounded-md bg-blue-200"
+          name=""
+          id=""
+          readOnly
+          value="2025-05-05"
+        />
+      </div>
+      <div className="flex justify-between items-center mb-2">
+        <label className="">Start Time:</label>
+        <input
+          type="time"
+          className="w-2/3 px-3 py-2 border border-gray-300 rounded-md bg-blue-200"
+          name=""
+          id=""
+          readOnly
+          value="17:00"
+        />
+      </div>
+      <div className="flex justify-between items-center mb-2">
+        <label className="">End Time:</label>
+        <input
+          type="time"
+          className="w-2/3 px-3  py-2 border border-gray-300 rounded-md bg-blue-200"
+          name=""
+          id=""
+          readOnly
+          value="20:00"
+        />
+      </div>
+      <div className="flex justify-between items-center mb-2">
+        <label className="">Spot:</label>
+        <input
+          type="text"
+          className="w-2/3 px-3  py-2 border border-gray-300 rounded-md bg-blue-200"
+          name=""
+          id=""
+          readOnly
+          value="R4-C2"
+        />
+      </div>
     </>
   );
 };
