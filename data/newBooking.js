571b22d5705867c4410506ec4557b5d279186703
chritophe1213 <christophe1213.com@gmail.com>
2025-05-16 15:31:17 +0300

    create state management for a booking

---
+++ data/newBooking.js
@@ -0,0 +1,36 @@
+import { createSlice } from "@reduxjs/toolkit";
+const initialState={
+    id:'',
+    starTime:'ss',
+    endTime:'',
+    spot:[
+      
+    ],
+}
+
+const newBookingSlice=createSlice({
+    name:'newBooking',
+    initialState,
+    reducers:{
+        setReservation:(state,action)=>{
+            return action.payload
+        },
+        addSpot:(state,action)=>{
+            state.spot = [...state.spot, action.payload]
+        },
+        setStartime:(state,action)=>{
+            const d={...state,starTime:action.payload}
+            return d
+        },
+        setEndTime:(state,action)=>{
+            const d={...state,endTime:action.payload}
+            return d
+        },
+        
+        deleteSpot:(state,action)=>{
+            state.spot=state.spot.filter(s=>s.id!==action.payload.id)
+        }
+    }
+})
+export const {setReservation,addSpot,deleteSpot,setStartime,setEndTime}=newBookingSlice.actions
+export default newBookingSlice.reducer
